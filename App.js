61d7d738a0110d8f6058370a48c3b0600c9f140a
Shubham's <sk192196@gmail.com>
2022-09-23 21:09:18 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Wallet from './wallet';
+
+
+function App() {
+  return (
+    <section className="main-cointainer">
+    <div className="App">
+      <Wallet/>
+      </div>
+      </section>
+  );
+}
+
+export default App;
